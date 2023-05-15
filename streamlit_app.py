6f789b251c7c41ef9822b3668681a3d89194402e
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2023-05-15 15:35:14 +0530

    Update streamlit_app.py
    Added csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title("🥣 My parents healthy new diner")
+
 streamlit.header("🥗 Breakfast Menu")
 streamlit.text("🐔 Omega 3 & raspberry Oatmeal")
 streamlit.text("🥑 Kale, Spinach & Rocket smoothie")
 streamlit.text("🍞 Hard-boiled Free-rage egg")
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
