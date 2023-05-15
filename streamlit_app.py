2337258a06ea671b68d4eff0c2801d56bdff8a32
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2023-05-15 15:30:33 +0530

    added few more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My parents healthy new diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & raspberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket smoothie")
+streamlit.text("Hard-boiled Free-rage egg")
