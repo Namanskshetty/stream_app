7869aef587d3122fc862589a38801d7f1f9e0149
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2023-05-16 22:45:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("HFruit load list contains:")
 streamlit.dataframe(my_data_rows)
 add_my_fruit=streamlit.text_input('Add new fruit')
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values(`"+add_my_fruit="`)")
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values(`"+add_my_fruit+"`)")
 streamlit.write('Thanks for adding ', add_my_fruit)
 
