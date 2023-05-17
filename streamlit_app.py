ec266f93f7be8d2e2996b9df0038f2c582c1d1e5
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2023-05-17 09:09:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -26,24 +27,43 @@ else:
   
 streamlit.header("Fruityvice Fruit Advice!")
 #New api display
+try:
+  fruit_choice=streamlit.text_input("what fruit would you like information about?")
+  if not freuit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # it normilize the json function for the data
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # onverts the data into table
+    streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-# it normilize the json function for the data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# onverts the data into table
-streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("HFruit load list contains:")
 streamlit.dataframe(my_data_rows)
-add_my_fruit=streamlit.text_input('Add new fruit')
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('"+add_my_fruit+"')")
+#add_my_fruit=streamlit.text_input('Add new fruit')
 if len(add_my_fruit)!=0:
+  my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('"+add_my_fruit+"')")
   streamlit.write('Thanks for adding ', add_my_fruit)
 
 streamlit.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
